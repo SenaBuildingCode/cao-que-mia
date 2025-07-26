e77e14e9c3048db5f320588731f88eec9e7baef6
Mariana Sena <35273542+MariSena331@users.noreply.github.com>
2025-07-25 22:43:10 -0300

    chore: implementing header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <Image
+          src="/cao-que-mia-logo.png"
+          alt="Paiming logo"
+          width={110}
+          height={40}
+        />
+      </div>
+
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/" className={styles.active}>
+              Início
+            </Link>
+          </li>
+          <li>
+            <Link href="#">Sobre nós</Link>
+          </li>
+          <li>
+            <Link href="#">Galeria</Link>
+          </li>
+          <li>
+            <Link href="#">Contato</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <button className={styles.button}>Faça uma doação</button>
+    </header>
+  );
+};
+
+export default Header;
